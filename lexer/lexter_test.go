a16a165405a4388048fddcef7afe8665d7b5540b
mjhmatt <matt.hill@bigcommerce.com>
2023-10-23 14:26:34 +0700

    adding lexer with test

---
+++ lexer/lexter_test.go
@@ -0,0 +1,29 @@
+package lexer
+
+import (
+	"go_interpreter/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+{}(),;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "-"},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - token type wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] - literal type wrong, expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
+}
